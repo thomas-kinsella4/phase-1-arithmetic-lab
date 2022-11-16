30ff1474c3c52297d6f0f46ae964bc8f9b3e569b
Thomas Kinsella <thomasjkinsella4@gmail.com>
2022-11-16 14:31:55 -0500

    commit tree 2db3208d1dca6449b075423dce3393f37df4a387

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+let num1 = 62;
+let num2 = 1;
+const multiply = num1 * num2;
+
+const random = (Math.floor(Math.random() * 10 ) + 1);
+
+let num3 = 20;
+let num4 = 16;
+const mod = num3 % num4;
+
+const max = Math.max(1, 20);
